8921666b935aa5a5011ff2284ee3310a05ac3349
BryanR125 <160572765+BryanR125@users.noreply.github.com>
2024-02-22 14:32:39 -0800

    Add files via upload

---
+++ Lab3_CF.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/***
+* {Controp Flow
+***/
+
+#include <iostream>
+#include <cstdlib> 
+using namespace std;
+
+int main() {
+  //numerical
+  int x = 0;
+  int y = 5; 
+
+  while(x<y){
+    printf("%i\n",x);
+    x++; 
+  }
+  printf("Looped through 5 times\n" );
+}
